fa4b3ba650858f89f85f93b6e4ac3e21459a97af
Binh Pham <60741317+pham-tuan-binh@users.noreply.github.com>
2025-12-06 12:43:02 +0100

    Update data folder path in data_record.sh

--- data_record.sh
+++ data_record.sh
@@ -7,7 +7,7 @@ cd deploy_real
 robot_ip="192.168.123.164"
 # robot_ip="192.168.110.24"
 data_frequency=30
-data_folder="~/ws/data/
+data_folder="/home/developer/ws/TWIST2/data"
 
 python server_data_record.py --frequency ${data_frequency} --robot_ip ${robot_ip} --data_folder ${data_folder}
         
