ad1681c98461cd86e6182b74327a136ea42017bc
Binh Pham <60741317+pham-tuan-binh@users.noreply.github.com>
2025-12-06 12:43:39 +0100

    Change measure_fps to 10 and enable smooth option

--- teleop.sh
+++ teleop.sh
@@ -15,6 +15,6 @@ python xrobot_teleop_to_robot_w_hand.py --robot unitree_g1 \
              --actual_human_height $actual_human_height \
              --redis_ip $redis_ip \
              --target_fps 100 \
-             --measure_fps 1 \
-            #  --smooth \
+             --measure_fps 10 \
+             --smooth
             #  --pinch_mode
